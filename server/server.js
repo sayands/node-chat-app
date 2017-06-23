5bf22a99a50c4f3d227634c49088002057839a37
sayands1997 <sayandsarkar.1997@gmail.com>
2017-06-23 19:06:21 +0530

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -13,14 +13,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
     
-  socket.emit('newMessage', {
-       from : 'sayands',
-       text : 'Hey!!!',
-       createAt : 123
-  }); 
-    
-  socket.on('createMessage', (message) => {
+ socket.on('createMessage', (message) => {
       console.log('createMessage', message);
+      io.emit('newMessage', {
+          from : message.form,
+          text : message.text,
+          createdAt : new Date().getTime()
+      });
   });   
     
   socket.on('disconnect', () => {
