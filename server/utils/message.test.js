848707969e0a45bbcb73f9bf5983f618d42cf7e8
sayands1997 <sayandsarkar.1997@gmail.com>
2017-06-23 19:49:38 +0530

    Create generateMessage utility

---
+++ server/utils/message.test.js
@@ -0,0 +1,14 @@
+var expect = require('expect');
+var {generateMessage} = require('./message');
+
+describe('generateMessage', () => {
+    it('should generate correct message object', () => {
+        var from = "Sayan";
+        var text = "Hey"
+        res = generateMessage(from,text);
+        
+        expect(res.createdAt).toBeA('number');
+        expect(res).toInclude({from,text});
+        
+    });
+});
